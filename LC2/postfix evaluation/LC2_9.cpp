0e6d153f0d91cdeefe32c090405b09d22082f9d0
rishaaanp <147536759+rishaaanp@users.noreply.github.com>
2024-05-05 16:50:06 +0530

    Add files via upload

---
+++ LC2/postfix evaluation/LC2_9.cpp
@@ -0,0 +1,29 @@
+#include"LC2_9.h"
+
+template<typename T>
+void Stack<T> :: push(T element) {
+    data.push(element);
+}
+
+template<typename T>
+T Stack<T> :: pop() {
+    if (isEmpty()) {
+        throw StackException("Stack is empty");
+    }
+    T topElement = data.top();
+    data.pop();
+    return topElement;
+}
+
+template<typename T>
+T Stack<T> :: peek() {
+    if (isEmpty()) {
+        throw StackException("Stack is empty");
+    }
+    return data.top();
+}
+
+template<typename T>
+bool Stack<T> :: isEmpty() {
+    return data.empty();
+}
---
+++ LC2/postfix evaluation/LC2_9.h
@@ -0,0 +1,29 @@
+#include<iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+class StackException {
+private:
+    string message;
+
+public:
+    StackException(const string& msg) {
+        message = msg;
+    }
+    const string& what() const { 
+        return message; 
+    }
+};
+
+template<typename T>
+class Stack {
+private:
+    stack<T> data;
+
+public:
+    void push(T element);
+    T pop();
+    T peek();
+    bool isEmpty();
+};
